bbe7644cf0632ef38d190c80cf0341c3c2164f31
kartud <1075238+kartud@users.noreply.github.com>
2022-08-07 16:44:14 -0700

    Partials (header/footer)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,12 +4,18 @@ const hbs = require('hbs');
 
 const app = express();
 
-// Serve static assets from the public folder.
+// Define paths for Express config.
 const publicDirectoryPath = path.join(__dirname, '../public');
-app.use(express.static(publicDirectoryPath));
+const viewsPath = path.join(__dirname, '../templates/views'); // Default is /views
+const partialsPath = path.join(__dirname, '../templates/partials');
 
-// Set dynamic handler (for dynamic content/templates). References templates in "views" folder.
+// Set up handlebars engine and views location (for dynamic content/templates).
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath); // Note: hbs., not app.
+
+// Set up directory for static content.
+app.use(express.static(publicDirectoryPath));
 
 // While static pages don't need routes specified, dynamic pages do.
 // Note: Static version will trump dynamic version (i.e., if there's an index.html in public/, it will be served)
